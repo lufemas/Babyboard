16496962352ddd35f85a0e860b98f77045d7c6c6
lufemas <maschietto.jr@gmail.com>
2025-09-04 19:51:41 -0300

    local x prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from "@tailwindcss/vite";
 
-// const isProd = process.env.NODE_ENV === 'production';
-// const isPages = !!process.env.GITHUB_PAGES; // we'll set this in the Action
-// console.log('isProd: ', isProd);
-// console.log('isPages: ', isPages);
+const isProd = process.env.NODE_ENV === 'production';
+const isPages = !!process.env.GITHUB_PAGES; // we'll set this in the Action
+console.log('isProd: ', isProd);
+console.log('isPages: ', isPages);
 export default defineConfig({
   output: 'static',
   integrations: [mdx(), react()],
   vite: {    plugins: [tailwind()]  },
-  // site:  isProd && isPages ? 'https://lufemas.github.io' : undefined,
-  // base:  isProd && isPages ? '/Babyboard/' : '/',   // '/' in dev
+  site:  isProd && isPages ? 'https://lufemas.github.io' : undefined,
+  base:  isProd && isPages ? '/Babyboard/' : '/',   // '/' in dev
 });
